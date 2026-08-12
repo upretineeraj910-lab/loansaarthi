1c88846f41839da60b6d5be43f3ed7ab4eea538d
neeraj upreti <upreti.neeraj98@gmail.com>
2026-08-12 16:56:54 +0530

    contact route

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -51,7 +51,7 @@ export async function POST(request: Request) {
 
     const body = await request.json();
 
-    console.log('📩 Contact form data received:', body);
+    // console.log('📩 Contact form data received:', body);
 
     const {
       fullName,
