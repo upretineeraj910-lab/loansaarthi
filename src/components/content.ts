aa0224e18603756fb03ba924b34a0a914d6730d7
Neeraj Upreti <upretineeraj910@gmail.com>
2026-09-17 10:11:25 +0530

    poonawalla image

--- src/components/content.ts
+++ src/components/content.ts
@@ -135,9 +135,8 @@ export const BANKS = [
   { name: "Bajaj Finserv", logo: "/banks/Bajaj Finserv.png" },
   { name: "Kotak Mahindra Bank", logo: "/banks/KOTAK.svg" },
   { name: "Tata Capital", logo: "/banks/TATA CAPITAL.jpeg" },
-  { name: "Aditya Birla Capital", logo: "/banks/ADITYA BIRLA.svg" }, // Apne file extension ke hisaab se check kar lein
+  { name: "Aditya Birla Capital", logo: "/banks/ADITYA BIRLA.svg" }, 
   { name: "Piramal Finance", logo: "/banks/PIRAMAL FINANCE.svg" },
   { name: "L&T Finance", logo: "/banks/L&T.png" },
-  { name: "Poonawalla", logo: "/banks/poonawalla_Fincorp.svg" },
-
+  { name: "Poonawalla", logo: "/banks/Poonawalla_Fincorp.svg" },
 ];
